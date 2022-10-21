c4cf3d367d2338040cc99e7a8a11c8f99a979d49
saifmhr <saifmahar165@gmail.com>
2022-10-21 17:44:38 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+
+public class Employee
+{
+    public string FirstName;
+    public string LastName;
+
+    public void PrintFullNAme()
+    {
+        Console.WriteLine(FirstName + " " + LastName);
+    }
+}
+
+
+public class PartTimeEmployee:Employee
+{
+
+    //use new keyword to hide
+    public new void PrintFullNAme()
+    {
+        base.PrintFullNAme();
+       // Console.WriteLine(FirstName + " " + LastName + " - constructor");
+    }
+}
+
+public class FullTimeEmployee:Employee
+{
+
+}
+public  class Program
+    {
+   public static void Main()
+        {
+         FullTimeEmployee FTE = new FullTimeEmployee();
+        FTE.FirstName = "Full Time ";
+        FTE.LastName = "Employee";
+        FTE.PrintFullNAme();
+
+
+        Employee PTE = new PartTimeEmployee();
+       // PartTimeEmployee PTE =new PartTimeEmployee();
+        PTE.FirstName = "Part Time ";
+        PTE.LastName = "Employee";
+        PTE.PrintFullNAme();
+       // ((Employee)PTE).PrintFullNAme();  //to show parent class
+        }
+    }
